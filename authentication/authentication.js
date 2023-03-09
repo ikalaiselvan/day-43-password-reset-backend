a29acbfbf7fdb623e198a049a281ccbe5007b0e7
Kalaiselvan <ikalaiselvan@users.noreply.github.com>
2023-03-09 15:26:38 +0530

    url changed in reset password link

--- authentication/authentication.js
+++ authentication/authentication.js
@@ -130,7 +130,7 @@ router.post("/forgotPassword", async function (request, response) {
         createdAt: new Date(),
       });
 
-    const link = `http://localhost:3000/passwordReset?token=${newToken}&id=${existingUser._id.toString()}`;
+    const link = `https://password-reset-flow-react.netlify.app/passwordReset?token=${newToken}&id=${existingUser._id.toString()}`;
 
     await sendEmail(existingUser.email, "Password Reset Link : ", {
       name: existingUser.name,
